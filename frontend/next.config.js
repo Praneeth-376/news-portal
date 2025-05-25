b41f83642afeb7faa14c40de363c9c0c5fe37b48
Praneeth-376 <pandalapraneeth376@gmail.com>
2025-05-25 15:34:28 +0530

    deleted:    frontend/app/main/layout.js 	modified:   frontend/app/page.js 	modified:   frontend/next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    appDir: true, // important for using /app directory
+  },
 };
 
 module.exports = nextConfig;
